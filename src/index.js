a426f2527cc02ccc11afe13b55394a8a8279f285
Tiffany <Tafby88@gmail.com>
2019-03-25 17:48:51 -0700

    made a show page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 			})
 			.then((json) => {
 				DATAS = json;
-				console.log(json);
 				renderImages(DATAS);
 			});
 	}
@@ -23,14 +22,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 		for (let i of DATAS) {
 			if (i.images.length > 0) {
-				const card = document.createElement('div');
-				card.className = 'card';
-				const cardBody = document.createElement('div');
-				cardBody.className = 'card-body';
-				mainCard.appendChild(card);
-
 				for (let imgObj of i.images) {
+					const card = document.createElement('div');
+					card.className = 'card';
+					const cardBody = document.createElement('div');
+					cardBody.className = 'card-body';
+					mainCard.appendChild(card);
+
 					let img = document.createElement('img');
+					img.addEventListener('click', () => {
+						renderShowPage(imgObj, i);
+					});
 					let p = document.createElement('p');
 					p.className = 'card-text';
 					img.className = 'card-img-top';
@@ -39,9 +41,60 @@ window.addEventListener('DOMContentLoaded', (event) => {
 					card.appendChild(img);
 					card.appendChild(cardBody);
 					cardBody.appendChild(p);
-					console.log('img', imgObj.img_url);
 				}
 			}
 		}
+	}
+
+	function renderShowPage(imgObj, data) {
+		document.getElementById('main-card-group').style.visibility = 'hidden';
+
+		//the new card for a single image
+		let body = document.body;
+		let cardMb = document.createElement('div');
+		cardMb.className = 'card mb-3';
+		let row = document.createElement('div');
+		row.className = 'row no-gutters';
+		let colOne = document.createElement('div');
+		colOne.className = 'col-md-4';
+		let colTwo = document.createElement('div');
+		colTwo.classList = 'col-md-8';
+		let cardBody = document.createElement('div');
+		cardBody.className = 'card-body';
+		let img = document.createElement('img');
+		img.className = 'card-img';
+		let cardCaption = document.createElement('h5');
+		cardCaption.className = 'card-title';
+
+		//these elements will be on the right side of the single image card
+		let userImg = document.createElement('img');
+		let userName = document.createElement('p');
+		let time = document.createElement('p');
+		let likeButton = document.createElement('button');
+
+		//comment elements
+		let form = document.createElement('form');
+		form.id = 'comment-form';
+		let textInput = document.createElement('input');
+		let submit = document.createElement('input');
+		submit.type = 'submit';
+
+		//appending new card
+		body.appendChild(cardMb);
+		cardMb.appendChild(row);
+		row.appendChild(colOne);
+		colOne.appendChild(img);
+		row.appendChild(colTwo);
+		colTwo.appendChild(cardBody);
+		cardBody.appendChild(cardCaption);
+
+		//text fill in
+		img.src = imgObj.img_url;
+		cardCaption.textInput = imgObj.caption;
+
+		userImg = data.avatar;
+		userName = data.name;
+		time = imgObj.created_at;
+		likeButton.textContent = `Like: ${imgObj.likes}`;
 	}
 });
